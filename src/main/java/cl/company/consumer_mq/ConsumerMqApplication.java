99cf9875809a9100d13ce283b40bcee490bdba34
matfuentesv <mat.fuentes@duocuc.cl>
2025-01-28 16:45:05 -0300

    commit initial

---
+++ src/main/java/cl/company/consumer_mq/ConsumerMqApplication.java
@@ -0,0 +1,13 @@
+package cl.company.consumer_mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/company/consumer_mq/ConsumerMqApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.company.consumer_mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
